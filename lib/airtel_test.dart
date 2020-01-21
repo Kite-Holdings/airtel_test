34e6afc27f4891897b2793d564c6446881c38918
Crazy Genius <elyudde@gmail.com>
2020-01-21 13:34:57 +0300

    initial

---
+++ lib/airtel_test.dart
@@ -0,0 +1,11 @@
+/// airtel_test
+///
+/// A Aqueduct web server.
+library airtel_test;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
